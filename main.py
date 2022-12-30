f81d635cf2a1f03d8239c916faf49ed0090e3e7b
Your Name <you@example.com>
2022-12-29 20:40:31 -0600

    part 2

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import datetime
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import yfinance as yf
+import requests
+
 
 
 def trade_spy(data, clf):
@@ -39,48 +41,110 @@ def trade_spy(data, clf):
   return profit
 
 
-# get the stock data for SPY
-spy = yf.Ticker("SPY")
 
-# get the current date and time
-now = datetime.datetime.now()
+def get_usa_spending_data():
+  url = "https://api.usaspending.gov"
 
-# calculate the start date as 3 years ago
-start_date = now - datetime.timedelta(days=365*3)
+  endpoint= "/api/v2/references/toptier_agencies/"
+  response = requests.get(f"{url}{endpoint}")
+  data = response.json()
+  toptier_agencies_df = pd.DataFrame(data["results"])
+  # print(toptier_agencies_df["agency_id"], toptier_agencies_df["agency_name"])
+  toptier_agencies_list = toptier_agencies_df["agency_id"].to_list()
+  # print(toptier_agencies_list)
 
-# calculate the end date as 3 months ago
-end_date = now - datetime.timedelta(days=90)
+  all_spending_df = pd.DataFrame()
+  for toptier_agencies_id in toptier_agencies_list:
+    endpoint = "/api/v2/award_spending/recipient"
+    page = 1
+    LIMIT = 500
 
-# retrieve the historical price data for the past 2 years, excluding the most recent 3 months
-data = spy.history(start=start_date, end=end_date)
+    results_df = pd.DataFrame()
+    query_more_pages = True
+    while(query_more_pages):
+      payload = {"awarding_agency_id": toptier_agencies_id, "fiscal_year": 2021, "limit": LIMIT, "page": page}
+      
 
-# create a new column with the label 'up' if the price increased from the previous day, 'down' if it decreased, and 'flat' if it remained the same
-data['label'] = np.where(data['Close'].shift(-1) > data['Close'], 'up',
-                         np.where(data['Close'].shift(-1) < data['Close'], 'down', 'flat'))
+      response = requests.get(f"{url}{endpoint}", params=payload)
+      if response.status_code == 200:
+        try:
+          data = response.json()
+          df = pd.DataFrame(data["results"])
+          if df.shape[0] == 0:
+            query_more_pages = False
+          
+          results_df = pd.concat([results_df, df], ignore_index=True)
+          page += 1
+        except KeyError:
+          query_more_pages = False
+        except:
+          query_more_pages = False
+      else:
+        query_more_pages = False
+
+    print(results_df)
+
+    all_spending_df = pd.concat([all_spending_df, results_df], ignore_index=True)
+    
+  # print(all_spending_df)
+  all_spending_df.to_csv("all_spending.csv", index=False)
+
+  return all_spending_df
 
 
-# create a feature matrix with the 'Open', 'High', 'Low', and 'Close' columns
-X = pd.DataFrame(data[['Open', 'High', 'Low', 'Close']], columns=['Open', 'High', 'Low', 'Close'])
+from os.path import exists
 
-# create a target vector with the 'label' column
-y = data['label']
+file_exists = exists("all_spending.csv")
+print(file_exists)
+
+if file_exists:
+  all_spending_df = pd.read_csv("all_spending.csv")
+else:
+  all_spending_df = get_usa_spending_data()
 
 
-# split the data into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # get the stock data for SPY
+# spy = yf.Ticker("SPY")
 
-# create a random forest classifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+# # get the current date and time
+# now = datetime.datetime.now()
 
-# fit the classifier to the training data
-clf.fit(X_train, y_train)
+# # calculate the start date as 3 years ago
+# start_date = now - datetime.timedelta(days=365*3)
 
-# evaluate the classifier on the test data
-accuracy = clf.score(X_test, y_test)
+# # calculate the end date as 3 months ago
+# end_date = now - datetime.timedelta(days=90)
 
-# run the trading algorithm
-profit = trade_spy(data, clf)
+# # retrieve the historical price data for the past 2 years, excluding the most recent 3 months
+# data = spy.history(start=start_date, end=end_date)
 
-# print the profit
-print(profit)
-print(f'Test accuracy: {accuracy:.2f}')
+# # create a new column with the label 'up' if the price increased from the previous day, 'down' if it decreased, and 'flat' if it remained the same
+# data['label'] = np.where(data['Close'].shift(-1) > data['Close'], 'up',
+#                          np.where(data['Close'].shift(-1) < data['Close'], 'down', 'flat'))
+
+
+# # create a feature matrix with the 'Open', 'High', 'Low', and 'Close' columns
+# X = pd.DataFrame(data[['Open', 'High', 'Low', 'Close']], columns=['Open', 'High', 'Low', 'Close'])
+
+# # create a target vector with the 'label' column
+# y = data['label']
+
+
+# # split the data into a training set and a test set
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# # create a random forest classifier
+# clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# # fit the classifier to the training data
+# clf.fit(X_train, y_train)
+
+# # evaluate the classifier on the test data
+# accuracy = clf.score(X_test, y_test)
+
+# # run the trading algorithm
+# profit = trade_spy(data, clf)
+
+# # print the profit
+# print(profit)
+# print(f'Test accuracy: {accuracy:.2f}')
